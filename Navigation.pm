e934af5a941afc677e3843b3c4abcd9f40ba28c3
chicagoist <le.legioner@gmail.com>
2022-05-03 13:54:19 +0300

    learn namespaces

---
+++ Navigation.pm
@@ -0,0 +1,33 @@
+package Navigation;
+use v5.10;
+# use CGI;
+# use POSIX;
+# use Encode qw(decode_utf8);
+# use Encode qw(decode encode);
+#= BEGIN{@ARGV=map Encode::decode(#\$_,1),@ARGV;}
+# BEGIN{@ARGV = map decode_utf8(#\$_, 1), @ARGV;}
+# use open qw(:std :encoding(UTF-8));
+use utf8::all 'GLOBAL';
+# use Encode::Locale;
+# use Encode;
+# use diagnostics;
+use strict;
+use warnings FATAL => 'all';
+use autodie qw(:all);
+use utf8;
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+use Data::Dumper;
+use Bundle::Camelcade;# for Intellij IDEA
+use YAML;
+use DDP;
+
+my @homeport = qw(Fuck Bitch);
+
+sub turn_toward_port {
+    my $f = "fuck";
+ return $f;
+}
+p @homeport;
+
+1;
