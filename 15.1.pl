5bc78b6535fdf4a829d98e59e6ec35e1a412f902
chicagoist <31587029+chicagoist@users.noreply.github.com>
2022-05-31 21:19:17 +0200

    use Exporter qw(import);

---
+++ 15.1.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+use 5.10.0;
+
+use FindBin qw($Bin);
+use lib "$Bin/ch15"; # в подкаталоге
+
+# use CGI;
+use Encode qw(decode_utf8);
+use Encode qw(decode encode);
+BEGIN{ $_ = map Encode::decode(\$_, 1) }
+# BEGIN{@ARGV = map decode_utf8(#\$_, 1), @ARGV;}
+use open qw(:std :encoding(UTF-8));
+use utf8::all 'GLOBAL';
+use Encode::Locale;
+# use diagnostics;
+
+
+use strict;
+use warnings FATAL => 'all';
+use IPC::System::Simple;
+use autodie qw(:all);
+use utf8;
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+
+use Data::Dumper;
+use Bundle::Camelcade;# for Intellij IDEA
+use POSIX ();
+use File::Spec;
+use Cwd;
+
+# File 15.1.pl
+# https://github.com/chicagoist/Intermediate-Perl-2nd-Edition.git
+# https://www.learning-perl.com/
+# https://www.linkedin.com/in/legioneroff/
+
+# Возьмите библиотеку Oogaboogoo, созданную вами в упражнении 1 главы 10,
+# и превратите ее в модуль, который можно будет подключать с помощью директивы use.
+# Измените программный код, обращающийся к библиотеке, таким образом, чтобы он мог
+# использовать импортированные подпрограммы (без указания полного имени подпрограмм,
+# включающих имя модуля) и проверьте его.
+
+use Date_ch15 qw(:all);
+
+my $date_ch15 = Date_ch15->new();
+# print "Name of day: ",$date_ch15->number_to_day_name(5),"\n";
+# print "Name of month: ",$date_ch15->number_to_mon_name(6),"\n";
+# print $date_ch15->today."\n";
+print "Name of day: ",number_to_day_name(5),"\n";
+print "Name of month: ",number_to_mon_name(6),"\n";
+print today()."\n";
+
+print date_10();
+
+=begin text
+
+$ perl 15.1.pl
+
+
+
+=end text
+
+#
+
+=cut
+
---
+++ ch15/Date_ch15.pm
@@ -0,0 +1,91 @@
+package Date_ch15 {
+    use v5.10;
+    use FindBin qw($Bin);
+    use lib "$Bin"; # в подкаталоге
+    our $VERSION = '0.01';
+    # use CGI;
+    # use POSIX;
+    # use Encode qw(decode_utf8);
+    # use Encode qw(decode encode);
+    #= BEGIN{@ARGV=map Encode::decode(#\$_,1),@ARGV;}
+    # BEGIN{@ARGV = map decode_utf8(#\$_, 1), @ARGV;}
+    # use open qw(:std :encoding(UTF-8));
+    use utf8::all 'GLOBAL';
+    # use Encode::Locale;
+    # use Encode;
+    # use diagnostics;
+    use strict;
+    use warnings FATAL => 'all';
+    use autodie qw(:all);
+    use utf8;
+    binmode(STDIN, ':utf8');
+    binmode(STDOUT, ':utf8');
+    use Data::Dumper;
+    use Bundle::Camelcade; # for Intellij IDEA
+    use YAML;
+    use DDP;
+
+    # use Moose;
+    # use namespace::autoclean;
+    #
+    # __PACKAGE__->meta->make_immutable;
+
+    #use base qw(Exporter);
+    use Exporter qw(import);
+    our @EXPORT = qw(today);
+    our @EXPORT_OK = qw(
+        number_to_day_name
+        number_to_mon_name
+        date_10
+    );
+
+    our %EXPORT_TAGS = (
+        all => [ @EXPORT, @EXPORT_OK ],
+        today => [ qw( today date_10 ) ],
+        days => [ qw(number_to_day_name) ],
+        months => [ qw(number_to_mon_name) ],
+    );
+
+    sub new { bless [], shift };
+
+    our @day = qw(арк дип уап сен поп сеп кир);
+    our @month = qw(диз под бод род сип уакс лин сен кун физ нап деп);
+
+    sub number_to_day_name {
+        #my $self = shift;
+        my $num = shift;
+        $num -= 1;
+        if ($num >= 0 && $num <= 6) {
+            return $day[$num];
+        }
+        else {
+            print "Invalid number";
+            exit;
+        }
+
+    }
+
+    sub number_to_mon_name {
+        #my $self = shift;
+        my $num = shift;
+        $num -= 1;
+        die $num + 1, " is not a valid month number" unless $num >= 0 and $num <= 11;
+        return $month[$num];
+    }
+
+    sub today {
+        my $self = shift;
+        my($sec, $min, $hour, $mday, $mon, $year, $wday) = localtime;
+        my $today = sprintf("Сегодня %s, %s %d, %d", number_to_day_name($wday), number_to_mon_name($mon), $mday, 1900+$year);
+        return $today;
+    }
+
+
+    sub date_10 {
+        #my $self = shift;
+        my $date_today = localtime()."\n";
+        return $date_today;
+    }
+
+}
+1;
